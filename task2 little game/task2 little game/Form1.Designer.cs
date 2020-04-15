06af33b0652794f91d7e1b722b22e2360dfb5a6b
pfefferberh <pfefferberhorion@gmail.com>
2020-04-15 20:57:42 +0300

    did hm

---
+++ task2 little game/task2 little game/Form1.Designer.cs
@@ -0,0 +1,98 @@
+﻿namespace task2_little_game
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.Start = new System.Windows.Forms.Button();
+            this.Stop = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // Start
+            // 
+            this.Start.Location = new System.Drawing.Point(69, 261);
+            this.Start.Name = "Start";
+            this.Start.Size = new System.Drawing.Size(106, 44);
+            this.Start.TabIndex = 0;
+            this.Start.Text = "Start";
+            this.Start.UseVisualStyleBackColor = true;
+            this.Start.Click += new System.EventHandler(this.Start_Click);
+            // 
+            // Stop
+            // 
+            this.Stop.Location = new System.Drawing.Point(210, 261);
+            this.Stop.Name = "Stop";
+            this.Stop.Size = new System.Drawing.Size(112, 44);
+            this.Stop.TabIndex = 1;
+            this.Stop.Text = "Stop";
+            this.Stop.UseVisualStyleBackColor = true;
+            this.Stop.Click += new System.EventHandler(this.Stop_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // label1
+            // 
+            this.label1.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.label1.Location = new System.Drawing.Point(0, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(68, 57);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "label";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.ClientSize = new System.Drawing.Size(384, 361);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.Stop);
+            this.Controls.Add(this.Start);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MinimumSize = new System.Drawing.Size(80, 80);
+            this.Name = "Form1";
+            this.Text = "Timer";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button Start;
+        private System.Windows.Forms.Button Stop;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ task2 little game/task2 little game/Form1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace task2_little_game
+{
+    public partial class Form1 : Form
+    {
+        Point point = new Point(0, 0);
+        int tim = 0;
+        public Form1()
+        {
+            InitializeComponent();
+            
+        }
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            this.label1.Text = tim++ + "s";
+           if (this.label1.Bounds.Right<this.Width-10 && this.label1.Location.Y == 0)
+                this.label1.Location = new Point(point.X+=10, point.Y);
+
+            else if(this.label1.Bounds.Right >= this.Width-20 && this.label1.Location.Y < this.Height-90)
+                this.label1.Location = new Point(point.X , point.Y+=10);
+
+            else if(this.label1.Location.X > 0 && this.label1.Location.Y <= this.Height-70)
+                this.label1.Location = new Point(point.X-=10, point.Y );
+
+            else if(this.label1.Location.X == 0 && this.label1.Location.Y >0)
+                this.label1.Location = new Point(point.X , point.Y-=10);
+        }
+
+        private void Start_Click(object sender, EventArgs e)
+        {
+            this.timer1.Start();
+        }
+
+        private void Stop_Click(object sender, EventArgs e)
+        {
+            this.timer1.Stop();
+        }
+    }
+}
