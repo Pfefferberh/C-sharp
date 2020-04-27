09930c56f15de1f9a62e62d29f66c437a74c5490
pfefferberh <pfefferberhorion@gmail.com>
2020-04-27 21:13:38 +0300

    fix

---
+++ Calculator/Calculator/Calculator.Designer.cs
@@ -0,0 +1,394 @@
+﻿namespace Calculator
+{
+    partial class Calculator
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.D1 = new System.Windows.Forms.Button();
+            this.D2 = new System.Windows.Forms.Button();
+            this.D3 = new System.Windows.Forms.Button();
+            this.D4 = new System.Windows.Forms.Button();
+            this.D5 = new System.Windows.Forms.Button();
+            this.D6 = new System.Windows.Forms.Button();
+            this.D7 = new System.Windows.Forms.Button();
+            this.D8 = new System.Windows.Forms.Button();
+            this.D9 = new System.Windows.Forms.Button();
+            this.D0 = new System.Windows.Forms.Button();
+            this.Oemcomma = new System.Windows.Forms.Button();
+            this.F9 = new System.Windows.Forms.Button();
+            this.screen = new System.Windows.Forms.TextBox();
+            this.Add = new System.Windows.Forms.Button();
+            this.Subtract = new System.Windows.Forms.Button();
+            this.Divide = new System.Windows.Forms.Button();
+            this.Multiply = new System.Windows.Forms.Button();
+            this.resal = new System.Windows.Forms.Button();
+            this.Back = new System.Windows.Forms.Button();
+            this.Oemtilde = new System.Windows.Forms.Button();
+            this.Delete = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // D1
+            // 
+            this.D1.BackColor = System.Drawing.Color.Black;
+            this.D1.FlatAppearance.BorderColor = System.Drawing.Color.Yellow;
+            this.D1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.D1.ForeColor = System.Drawing.Color.Yellow;
+            this.D1.Location = new System.Drawing.Point(34, 140);
+            this.D1.Name = "D1";
+            this.D1.Size = new System.Drawing.Size(70, 34);
+            this.D1.TabIndex = 3;
+            this.D1.Text = "1";
+            this.D1.UseVisualStyleBackColor = false;
+            this.D1.MouseClick += new System.Windows.Forms.MouseEventHandler(this.but_MouseClick);
+            // 
+            // D2
+            // 
+            this.D2.BackColor = System.Drawing.Color.Black;
+            this.D2.FlatAppearance.BorderColor = System.Drawing.Color.Yellow;
+            this.D2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.D2.ForeColor = System.Drawing.Color.Yellow;
+            this.D2.Location = new System.Drawing.Point(110, 140);
+            this.D2.Name = "D2";
+            this.D2.Size = new System.Drawing.Size(70, 34);
+            this.D2.TabIndex = 4;
+            this.D2.Text = "2";
+            this.D2.UseVisualStyleBackColor = false;
+            this.D2.MouseClick += new System.Windows.Forms.MouseEventHandler(this.but_MouseClick);
+            // 
+            // D3
+            // 
+            this.D3.BackColor = System.Drawing.Color.Black;
+            this.D3.FlatAppearance.BorderColor = System.Drawing.Color.Yellow;
+            this.D3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.D3.ForeColor = System.Drawing.Color.Yellow;
+            this.D3.Location = new System.Drawing.Point(186, 140);
+            this.D3.Name = "D3";
+            this.D3.Size = new System.Drawing.Size(70, 34);
+            this.D3.TabIndex = 5;
+            this.D3.Text = "3";
+            this.D3.UseVisualStyleBackColor = false;
+            this.D3.MouseClick += new System.Windows.Forms.MouseEventHandler(this.but_MouseClick);
+            // 
+            // D4
+            // 
+            this.D4.BackColor = System.Drawing.Color.Black;
+            this.D4.FlatAppearance.BorderColor = System.Drawing.Color.Yellow;
+            this.D4.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.D4.ForeColor = System.Drawing.Color.Yellow;
+            this.D4.Location = new System.Drawing.Point(34, 86);
+            this.D4.Name = "D4";
+            this.D4.Size = new System.Drawing.Size(70, 34);
+            this.D4.TabIndex = 6;
+            this.D4.Text = "4";
+            this.D4.UseVisualStyleBackColor = false;
+            this.D4.MouseClick += new System.Windows.Forms.MouseEventHandler(this.but_MouseClick);
+            // 
+            // D5
+            // 
+            this.D5.BackColor = System.Drawing.Color.Black;
+            this.D5.FlatAppearance.BorderColor = System.Drawing.Color.Yellow;
+            this.D5.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.D5.ForeColor = System.Drawing.Color.Yellow;
+            this.D5.Location = new System.Drawing.Point(110, 86);
+            this.D5.Name = "D5";
+            this.D5.Size = new System.Drawing.Size(70, 34);
+            this.D5.TabIndex = 7;
+            this.D5.Text = "5";
+            this.D5.UseVisualStyleBackColor = false;
+            this.D5.MouseClick += new System.Windows.Forms.MouseEventHandler(this.but_MouseClick);
+            // 
+            // D6
+            // 
+            this.D6.BackColor = System.Drawing.Color.Black;
+            this.D6.FlatAppearance.BorderColor = System.Drawing.Color.Yellow;
+            this.D6.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.D6.ForeColor = System.Drawing.Color.Yellow;
+            this.D6.Location = new System.Drawing.Point(186, 86);
+            this.D6.Name = "D6";
+            this.D6.Size = new System.Drawing.Size(70, 34);
+            this.D6.TabIndex = 8;
+            this.D6.Text = "6";
+            this.D6.UseVisualStyleBackColor = false;
+            this.D6.MouseClick += new System.Windows.Forms.MouseEventHandler(this.but_MouseClick);
+            // 
+            // D7
+            // 
+            this.D7.BackColor = System.Drawing.Color.Black;
+            this.D7.FlatAppearance.BorderColor = System.Drawing.Color.Yellow;
+            this.D7.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.D7.ForeColor = System.Drawing.Color.Yellow;
+            this.D7.Location = new System.Drawing.Point(34, 31);
+            this.D7.Name = "D7";
+            this.D7.Size = new System.Drawing.Size(70, 34);
+            this.D7.TabIndex = 9;
+            this.D7.Text = "7";
+            this.D7.UseVisualStyleBackColor = false;
+            this.D7.MouseClick += new System.Windows.Forms.MouseEventHandler(this.but_MouseClick);
+            // 
+            // D8
+            // 
+            this.D8.BackColor = System.Drawing.Color.Black;
+            this.D8.FlatAppearance.BorderColor = System.Drawing.Color.Yellow;
+            this.D8.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.D8.ForeColor = System.Drawing.Color.Yellow;
+            this.D8.Location = new System.Drawing.Point(110, 31);
+            this.D8.Name = "D8";
+            this.D8.Size = new System.Drawing.Size(70, 34);
+            this.D8.TabIndex = 10;
+            this.D8.Text = "8";
+            this.D8.UseVisualStyleBackColor = false;
+            this.D8.MouseClick += new System.Windows.Forms.MouseEventHandler(this.but_MouseClick);
+            // 
+            // D9
+            // 
+            this.D9.BackColor = System.Drawing.Color.Black;
+            this.D9.FlatAppearance.BorderColor = System.Drawing.Color.Yellow;
+            this.D9.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.D9.ForeColor = System.Drawing.Color.Yellow;
+            this.D9.Location = new System.Drawing.Point(186, 31);
+            this.D9.Name = "D9";
+            this.D9.Size = new System.Drawing.Size(70, 34);
+            this.D9.TabIndex = 11;
+            this.D9.Text = "9";
+            this.D9.UseVisualStyleBackColor = false;
+            this.D9.MouseClick += new System.Windows.Forms.MouseEventHandler(this.but_MouseClick);
+            // 
+            // D0
+            // 
+            this.D0.BackColor = System.Drawing.Color.Black;
+            this.D0.FlatAppearance.BorderColor = System.Drawing.Color.Yellow;
+            this.D0.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.D0.ForeColor = System.Drawing.Color.Yellow;
+            this.D0.Location = new System.Drawing.Point(34, 195);
+            this.D0.Name = "D0";
+            this.D0.Size = new System.Drawing.Size(70, 34);
+            this.D0.TabIndex = 17;
+            this.D0.Text = "0";
+            this.D0.UseVisualStyleBackColor = false;
+            this.D0.MouseClick += new System.Windows.Forms.MouseEventHandler(this.but_MouseClick);
+            // 
+            // Oemcomma
+            // 
+            this.Oemcomma.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.Oemcomma.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.Oemcomma.ForeColor = System.Drawing.SystemColors.InactiveCaption;
+            this.Oemcomma.Location = new System.Drawing.Point(110, 195);
+            this.Oemcomma.Name = "Oemcomma";
+            this.Oemcomma.Size = new System.Drawing.Size(70, 34);
+            this.Oemcomma.TabIndex = 1;
+            this.Oemcomma.Text = ".";
+            this.Oemcomma.UseVisualStyleBackColor = true;
+            this.Oemcomma.MouseClick += new System.Windows.Forms.MouseEventHandler(this.but_option_MouseClick);
+            // 
+            // F9
+            // 
+            this.F9.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.F9.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.F9.ForeColor = System.Drawing.SystemColors.InactiveCaption;
+            this.F9.Location = new System.Drawing.Point(186, 195);
+            this.F9.Name = "F9";
+            this.F9.Size = new System.Drawing.Size(70, 34);
+            this.F9.TabIndex = 2;
+            this.F9.Text = "ChgSign";
+            this.F9.UseVisualStyleBackColor = true;
+            this.F9.MouseClick += new System.Windows.Forms.MouseEventHandler(this.but_MouseClick);
+            // 
+            // screen
+            // 
+            this.screen.Location = new System.Drawing.Point(290, 39);
+            this.screen.Name = "screen";
+            this.screen.ReadOnly = true;
+            this.screen.Size = new System.Drawing.Size(249, 20);
+            this.screen.TabIndex = 12;
+            this.screen.Text = "0";
+            this.screen.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            this.screen.TextChanged += new System.EventHandler(this.screen_TextChanged);
+            // 
+            // Add
+            // 
+            this.Add.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.Add.ForeColor = System.Drawing.SystemColors.HotTrack;
+            this.Add.Location = new System.Drawing.Point(290, 86);
+            this.Add.Name = "Add";
+            this.Add.Size = new System.Drawing.Size(66, 34);
+            this.Add.TabIndex = 13;
+            this.Add.Text = "+";
+            this.Add.UseVisualStyleBackColor = true;
+            this.Add.MouseClick += new System.Windows.Forms.MouseEventHandler(this.but_operat_MouseClick);
+            // 
+            // Subtract
+            // 
+            this.Subtract.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.Subtract.ForeColor = System.Drawing.SystemColors.HotTrack;
+            this.Subtract.Location = new System.Drawing.Point(389, 86);
+            this.Subtract.Name = "Subtract";
+            this.Subtract.Size = new System.Drawing.Size(66, 34);
+            this.Subtract.TabIndex = 14;
+            this.Subtract.Text = "-";
+            this.Subtract.UseVisualStyleBackColor = true;
+            this.Subtract.MouseClick += new System.Windows.Forms.MouseEventHandler(this.but_MouseClick);
+            // 
+            // Divide
+            // 
+            this.Divide.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.Divide.ForeColor = System.Drawing.SystemColors.HotTrack;
+            this.Divide.Location = new System.Drawing.Point(290, 140);
+            this.Divide.Name = "Divide";
+            this.Divide.Size = new System.Drawing.Size(66, 34);
+            this.Divide.TabIndex = 15;
+            this.Divide.Text = "/";
+            this.Divide.UseVisualStyleBackColor = true;
+            this.Divide.MouseClick += new System.Windows.Forms.MouseEventHandler(this.but_MouseClick);
+            // 
+            // Multiply
+            // 
+            this.Multiply.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.Multiply.ForeColor = System.Drawing.SystemColors.HotTrack;
+            this.Multiply.Location = new System.Drawing.Point(389, 140);
+            this.Multiply.Name = "Multiply";
+            this.Multiply.Size = new System.Drawing.Size(66, 34);
+            this.Multiply.TabIndex = 16;
+            this.Multiply.Text = "*";
+            this.Multiply.UseVisualStyleBackColor = true;
+            this.Multiply.MouseClick += new System.Windows.Forms.MouseEventHandler(this.but_MouseClick);
+            // 
+            // resal
+            // 
+            this.resal.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.resal.ForeColor = System.Drawing.SystemColors.HotTrack;
+            this.resal.Location = new System.Drawing.Point(486, 86);
+            this.resal.Name = "resal";
+            this.resal.Size = new System.Drawing.Size(53, 88);
+            this.resal.TabIndex = 0;
+            this.resal.Text = "=";
+            this.resal.UseVisualStyleBackColor = true;
+            this.resal.MouseClick += new System.Windows.Forms.MouseEventHandler(this.but_MouseClick);
+            // 
+            // Back
+            // 
+            this.Back.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.Back.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.Back.ForeColor = System.Drawing.SystemColors.InactiveCaption;
+            this.Back.Location = new System.Drawing.Point(290, 195);
+            this.Back.Name = "Back";
+            this.Back.Size = new System.Drawing.Size(75, 34);
+            this.Back.TabIndex = 18;
+            this.Back.Text = "BackSpace";
+            this.Back.UseVisualStyleBackColor = true;
+            this.Back.MouseClick += new System.Windows.Forms.MouseEventHandler(this.but_MouseClick);
+            // 
+            // Oemtilde
+            // 
+            this.Oemtilde.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.Oemtilde.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.Oemtilde.ForeColor = System.Drawing.SystemColors.InactiveCaption;
+            this.Oemtilde.Location = new System.Drawing.Point(380, 195);
+            this.Oemtilde.Name = "Oemtilde";
+            this.Oemtilde.Size = new System.Drawing.Size(75, 34);
+            this.Oemtilde.TabIndex = 19;
+            this.Oemtilde.Text = "BackSkr";
+            this.Oemtilde.UseVisualStyleBackColor = true;
+            this.Oemtilde.MouseClick += new System.Windows.Forms.MouseEventHandler(this.but_MouseClick);
+            // 
+            // Delete
+            // 
+            this.Delete.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.Delete.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.Delete.ForeColor = System.Drawing.SystemColors.InactiveCaption;
+            this.Delete.Location = new System.Drawing.Point(464, 195);
+            this.Delete.Name = "Delete";
+            this.Delete.Size = new System.Drawing.Size(75, 34);
+            this.Delete.TabIndex = 20;
+            this.Delete.Text = "ClearAll";
+            this.Delete.UseVisualStyleBackColor = true;
+            this.Delete.MouseClick += new System.Windows.Forms.MouseEventHandler(this.but_MouseClick);
+            // 
+            // Calculator
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(574, 253);
+            this.Controls.Add(this.Delete);
+            this.Controls.Add(this.Multiply);
+            this.Controls.Add(this.Oemtilde);
+            this.Controls.Add(this.Divide);
+            this.Controls.Add(this.Back);
+            this.Controls.Add(this.resal);
+            this.Controls.Add(this.Subtract);
+            this.Controls.Add(this.Add);
+            this.Controls.Add(this.screen);
+            this.Controls.Add(this.D9);
+            this.Controls.Add(this.D6);
+            this.Controls.Add(this.F9);
+            this.Controls.Add(this.D3);
+            this.Controls.Add(this.D8);
+            this.Controls.Add(this.D5);
+            this.Controls.Add(this.Oemcomma);
+            this.Controls.Add(this.D2);
+            this.Controls.Add(this.D7);
+            this.Controls.Add(this.D0);
+            this.Controls.Add(this.D4);
+            this.Controls.Add(this.D1);
+            this.KeyPreview = true;
+            this.MaximizeBox = false;
+            this.Name = "Calculator";
+            this.Text = "Calculator";
+            this.TopMost = true;
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.Calculator_KeyUp);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button D1;
+        private System.Windows.Forms.Button D2;
+        private System.Windows.Forms.Button D3;
+        private System.Windows.Forms.Button D4;
+        private System.Windows.Forms.Button D5;
+        private System.Windows.Forms.Button D6;
+        private System.Windows.Forms.Button D7;
+        private System.Windows.Forms.Button D8;
+        private System.Windows.Forms.Button D9;
+        private System.Windows.Forms.Button D0;
+        private System.Windows.Forms.Button Oemcomma;
+        private System.Windows.Forms.Button F9;
+        private System.Windows.Forms.TextBox screen;
+        private System.Windows.Forms.Button Add;
+        private System.Windows.Forms.Button Subtract;
+        private System.Windows.Forms.Button Divide;
+        private System.Windows.Forms.Button Multiply;
+        private System.Windows.Forms.Button resal;
+        private System.Windows.Forms.Button Back;
+        private System.Windows.Forms.Button Oemtilde;
+        private System.Windows.Forms.Button Delete;
+    }
+}
+
---
+++ Calculator/Calculator/Calculator.cs
@@ -0,0 +1,198 @@
+﻿using System;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace Calculator
+{
+    public partial class Calculator : Form
+    {
+        bool point_ex = true;//filter comms
+        bool bad_key = true;//the filter of doing the laundry by the keyboard
+
+        int temp = 0; //coma index tracking
+
+        string scrn_ = "";//to read values ​​from the screen
+        string math = "";//for math oprration
+
+        Double memb = 0;
+        Double memb_two = 0;
+        Double res = 0;
+
+        public Calculator()
+        {
+            InitializeComponent();
+        }
+
+        private void screen_TextChanged(object sender, EventArgs e)
+        {
+            if (bad_key)
+            {
+                res = Convert.ToDouble(scrn_);
+                if (!point_ex)
+                    screen.Text = scrn_;
+                else
+                    screen.Text = res.ToString();
+
+            }
+            bad_key = true;
+        }
+
+        private void but_MouseClick(object sender, MouseEventArgs e)
+        {
+            switch ((sender as Button).Name)
+            {
+                case "D0":
+                    scrn_ = screen.Text + "0";
+                    break;
+                case "D1":
+                    scrn_ = screen.Text + "1";
+                    break;
+                case "D2":
+                    scrn_ = screen.Text + "2";
+                    break;
+                case "D3":
+                    scrn_ = screen.Text + "3";
+                    break;
+                case "D4":
+                    scrn_ = screen.Text + "4";
+                    break;
+                case "D5":
+                    scrn_ = screen.Text + "5";
+                    break;
+                case "D6":
+                    scrn_ = screen.Text + "6";
+                    break;
+                case "D7":
+                    scrn_ = screen.Text + "7";
+                    break;
+                case "D8":
+                    scrn_ = screen.Text + "8";
+                    break;
+                case "D9":
+                    scrn_ = screen.Text + "9";
+                    break;
+                default:
+                    but_operat_MouseClick(sender, e);
+                    return;
+            }
+            screen_TextChanged(null, null);
+        }
+
+        private void but_operat_MouseClick(object sender, MouseEventArgs e)
+        {
+            switch ((sender as Button).Name)
+            {
+                case "Add":
+                case "Oemplus":
+                    math = "+";
+                    restart();
+                    break;
+                case "Subtract":
+                case "OemMinus":
+                    math = "-";
+                    restart();
+                    break;
+                case "Multiply":
+                    math = "*";
+                    restart();
+                    break;
+                case "Divide":
+                    math = "/";
+                    restart();
+                    break;
+                case "resal":
+                    memb_two = res;
+                    if (math == "+")
+                        res = memb + memb_two;
+                    else if (math == "-")
+                        res = memb - memb_two;
+                    else if (math == "*")
+                        res = memb * memb_two;
+                    else if (math == "/")
+                    {
+                        if (memb_two == 0)
+                            MessageBox.Show("Divade on 0 !\nEnter digit two!!");
+                        else
+                            res = memb / memb_two;
+                    }
+                    scrn_ = res.ToString();
+                    memb = 0;
+                    break;
+                default:
+                    but_option_MouseClick(sender, e);
+                    return;
+            }
+            screen_TextChanged(null, null);
+
+        }
+        private void but_option_MouseClick(object sender, MouseEventArgs e)
+        {
+            switch ((sender as Button).Name)
+            {
+                case "Oemcomma":
+                    if (point_ex)
+                    {
+                        temp = res.ToString().Length;
+                        scrn_ = res.ToString().Insert(temp, ",");
+                    }
+                    point_ex = false;
+                    break;
+                case "F9":
+                    res *= -1;
+                    scrn_ = res.ToString();
+                    break;
+
+                case "Back":
+                    if (scrn_.LastOrDefault() == ',')
+                    {
+                        scrn_ = scrn_.Remove(scrn_.Length - 1, 1);
+                        point_ex = true;
+                    }
+                    else if (scrn_.Length > 0)
+                        scrn_ = scrn_.Remove(scrn_.Length - 1, 1);
+                    if (scrn_.Length == 0 || scrn_ == "-")
+                    {
+                        scrn_ = "0";
+                        screen.Text = "";
+                        point_ex = true;
+                    }
+                    break;
+                case "Oemtilde":
+                    scrn_ = scrn_.Remove(0, scrn_.Length - 1);
+                    scrn_ = "0";
+                    point_ex = true;
+                    break;
+                case "Delete":
+                    point_ex = true;
+                    temp = 0;
+                    scrn_ = "0";
+                    math = "";
+                    memb = 0;
+                    memb_two = 0;
+                    res = 0;
+                    break;
+                default:
+                    bad_key = false;
+                    return;
+            }
+            screen_TextChanged(null, null);
+        }
+        private void Calculator_KeyUp(object sender, KeyEventArgs e)
+        {
+            Button button = new Button();
+            if (e.KeyValue == 13)
+                button.Name = "resal";
+            else
+                button.Name = e.KeyCode.ToString();
+
+            but_MouseClick(button, null);
+        }
+        void restart()
+        {
+            memb = res;
+            res = 0;
+            scrn_ = "0";
+            point_ex = true;
+        }
+    }
+}
